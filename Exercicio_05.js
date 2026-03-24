1277869e2d742b6f24be1e18f0b0399e78c68916
marcosvoltolini <marcos_voltolini-@estudante.sesisenai.org.br>
2026-03-24 08:00:33 -0300

    fix - arrumei erro na 5

--- Exercicio_05.js
+++ Exercicio_05.js
@@ -1,13 +1,13 @@
 function transformacao() {
-    let impar = 0
-    let par = 0
+    
 
     for (let i =0; i>0; i++);
-     numeros.push(Number parseInt(prompt("Digite um número: ")));
-    if (numeros % 2 == 0) {
-        numeros = numeros + 1;
+     let numero = parseInt(prompt("Digite um número:"));
+    if (numero % 2 == 0) {
+        numero = numero + 1;
     }else{
-        numeros = numeros + 1;
+        numero = numero + 1;
     }
-    console.log ("Seu numero é:" numeros );
+    alert ("Seu numero é:" + numero);
 }
+transformacao ()
