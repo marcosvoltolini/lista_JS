513ad010cf2f400aee1351db178c3335fdc86af0
marcosvoltolini <marcos_voltolini-@estudante.sesisenai.org.br>
2026-03-24 09:16:02 -0300

    feat - terminei a funçao do picole exercicio 7

---
+++ Exercicio_07.js
@@ -0,0 +1,24 @@
+function picole() {
+    let codigo
+    codigo = prompt("Digite um numero de 1 a 9, para os sabores de picolé:");
+
+    if (codigo == 1) {
+        alert("Chocolate R$1,50");
+    } else if (codigo == 2) {
+        alert("Morango R$2,50");
+    } else if (codigo == 3) {
+        alert("Creme R$2,50");
+    } else if (codigo == 4) {
+        alert("Manga R$3,20");
+    }else if (codigo == 5) {
+        alert ("Melancia R$3,40");
+    }else if (codigo == 6) {
+        alert ("Vanilla Ice R$3,00");
+    }else if (codigo == 7) {
+        alert ("Céu Azul R$3,60");
+    }else if (codigo == 8) {
+        alert ("Brownie R$4,00");
+    }else if (codigo == 9) {
+        alert ("Hawaiano R$5,00");
+    }
+}
