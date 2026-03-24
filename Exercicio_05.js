9733df9bc837994fc82281a4db3a6df91c0be74f
marcosvoltolini <marcos_voltolini-@estudante.sesisenai.org.br>
2026-03-24 07:49:45 -0300

    feat - terminei a 5

---
+++ Exercicio_05.js
@@ -0,0 +1,13 @@
+function transformacao() {
+    let impar = 0
+    let par = 0
+
+    for (let i =0; i>0; i++);
+     numeros.push(Number parseInt(prompt("Digite um número: ")));
+    if (numeros % 2 == 0) {
+        numeros = numeros + 1;
+    }else{
+        numeros = numeros + 1;
+    }
+    console.log ("Seu numero é:" numeros );
+}
