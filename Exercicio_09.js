e6bb7eb8c10c85004416cab1c8dfd1f1477da161
marcosvoltolini <marcos_voltolini-@estudante.sesisenai.org.br>
2026-03-28 13:00:58 -0300

    feat - terminei a 9

--- Exercicio_09.js
+++ Exercicio_09.js
@@ -1,3 +1,11 @@
-function ordenação () {
-    
+function ordenacao () {
+    let n1 = prompt("Digite o primeiro numero: ");
+    let n2 = prompt("Digite o segundo numero: ");
+
+    if (n1 > n2) {
+        console.log ("Ordenados maior para menor: " + n1, n2);
+
+    }else{
+        console.log ("Ordenados maior para menor: " + n2, n1);
+    }
 }
