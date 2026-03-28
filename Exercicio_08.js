7adc251cb00179ef3543271dee9ee758fd0c8330
marcosvoltolini <marcos_voltolini-@estudante.sesisenai.org.br>
2026-03-28 12:52:33 -0300

    feat - terminei a 8

---
+++ Exercicio_08.js
@@ -0,0 +1,10 @@
+function variaveis () {
+    let a = 2;
+    let b = 5;
+
+    alert ("valor de A: " + a + " valor de B: " + b);
+    alert ("diferença dos dois: "+ (a-b));
+    alert ("Dobro do primeiro mais triplo do segundo: " + (a*2+b*3));
+    alert ("Multiplicação dos dois: " + (a*b));
+    
+}
